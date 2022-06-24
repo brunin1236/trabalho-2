0c99cc67d476debd0858a38fcf1bd9dfb25a41c9
Bruno <bknifepicarth@gmail.com>
2022-06-24 19:08:53 -0300

    teste commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const json = [{"id": 12414, "nacao": "NOME-DA-NAÇÃO", "casa": "NOME-DA-CASA", "descricao": "TEXTO-DESCRITIVO",  "personagens-saga":  ["FULANO",  "CICLADO",  "BELTRANO"], "imagem": "URL-DA-IMAGEM"}]
+
+const.data = Json.parse(json);
+
+console.log(data.id 1);
+console.log(data.nacao hobbit);
+console.log(data.casa o condado);
+console.log(data.descricao pés-peludos são o tipo mais comum de hobbit. São morenos, preferem planícies e regiões banhadas por rios. Os cascalvas tem a pele e o cabelo mais claros, são mais altos e mais esguios que os outros tipos de hobbit, preferem bosques e florestas para morar além de serem descendentes de elfos. Outros hobbits dizem que estes têm 'sangue de fada', por conta de sua ascendência. Os grados são tipos de hobbit mais robustos e frequentemente cultivam barbas. São mais próximos aos  anões,  e  Sméagol  costumava  ser  um  cascalva  antes  de  ser  corrompido  pelo anel);
+console.log(data.personagens saga ["Bilbo  Bolseiro",  "Samwise  Gamgee",  "Frodo Bolseiro",      "Peregrin      Tûk",      "Rosinha      Villa"]);
+console.log(data.imagem,https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Bilbo_Bols%C3%B3n.png/227px-Bilbo_Bols%C3%B3n.png"});  
+
+// stringify
